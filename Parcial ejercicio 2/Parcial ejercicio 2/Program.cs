d967a5d360a9b7102ad62f0756388ff46c62ea89
LemuelAcosta <63744340+LemuelAcosta@users.noreply.github.com>
2020-06-02 22:51:01 -0300

    Add files via upload

---
+++ Parcial ejercicio 2/Parcial ejercicio 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Parcial_ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day;
+            Console.WriteLine("Deme el nombre de un dia de la semana.");
+            day = Console.ReadLine();
+
+           if (day == "viernes") 
+            {
+                Console.WriteLine("Es un dia del fin de semana");
+            }
+            if (day == "sabado")
+            {
+                Console.WriteLine("Es un dia del fin de semana");
+            }
+            if (day == "domigo")
+            {
+                Console.WriteLine("Es un dia del fin de semana");
+            }
+            if (day == "lunes")
+            {
+                Console.WriteLine("no es un dia del fin de semana");
+            }
+            if (day == "martes")
+            {
+                Console.WriteLine("no es un dia del fin de semana");
+            }
+            if (day == "miercoles")
+            {
+                Console.WriteLine("no es un dia del fin de semana");
+            }
+            if (day == "jueves")
+            {
+                Console.WriteLine("no es un dia del fin de semana");
+            }
+        }
+    }
+}
